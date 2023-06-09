fbef8c828d2fc46aeb5157f9b76844af730f7631
Guido Gloor <haslo@haslo.ch>
2023-06-09 23:47:39 +0200

    Drag and drop works with rocket model

--- Assets/Scripts/Managers/UIInterface.cs
+++ Assets/Scripts/Managers/UIInterface.cs
@@ -1,9 +1,10 @@
 using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class UIInterface : MonoBehaviour {
     private GameObject focusObject;
-    [SerializeField] private GameObject cubeTurretPrefab;
+    [SerializeField] private GameObject rocketTurretPrefab;
     private Camera mainCamera;
 
     private void Start() {
@@ -11,29 +12,32 @@ public class UIInterface : MonoBehaviour {
     }
 
     private void Update() {
-        if (Input.GetMouseButtonDown(0)) {
-            var ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        if (Input.GetMouseButtonDown(0)) { // if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Began)
+            var ray = mainCamera.ScreenPointToRay(Input.mousePosition); // Input.GetTouch(0).position
             if (!Physics.Raycast(ray, out var hit)) {
                 return;
             }
 
-            focusObject = Instantiate(cubeTurretPrefab, hit.point, cubeTurretPrefab.transform.rotation);
-            focusObject.GetComponentInChildren<Collider>().enabled = false;
-        } else if (focusObject && Input.GetMouseButton(0)) {
+            focusObject = Instantiate(rocketTurretPrefab, hit.point, rocketTurretPrefab.transform.rotation);
+            var focusCollider = focusObject.GetComponentInChildren<Collider>();
+            if (focusCollider) {
+                focusCollider.enabled = false;
+            }
+        } else if (focusObject && Input.GetMouseButton(0)) { // if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Moved)
             var ray = mainCamera.ScreenPointToRay(Input.mousePosition);
             if (!Physics.Raycast(ray, out var hit)) {
                 return;
             }
 
             focusObject.transform.position = hit.point;
-        } else if (focusObject && Input.GetMouseButtonUp(0)) {
+        } else if (focusObject && Input.GetMouseButtonUp(0)) { // if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Ended)
             var ray = mainCamera.ScreenPointToRay(Input.mousePosition);
             if (!Physics.Raycast(ray, out var hit))
                 return;
             var collidedObject = hit.collider.gameObject;
-            if (collidedObject.CompareTag("EmptyPlatform")) {
+            if (collidedObject.CompareTag("EmptyPlatform") && hit.normal == Vector3.up) {
                 var collidedPosition = collidedObject.transform.position;
-                focusObject.transform.position = new Vector3(collidedPosition.x, collidedPosition.y + 0.5f, collidedPosition.z);
+                focusObject.transform.position = new Vector3(collidedPosition.x, focusObject.transform.position.y, collidedPosition.z);
                 collidedObject.tag = "OccupiedPlatform";
             } else {
                 Destroy(focusObject);
