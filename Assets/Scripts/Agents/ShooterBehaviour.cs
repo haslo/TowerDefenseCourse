c9e6a9b1c719e961e863716357eb6a54ef31b6c2
Guido Gloor <haslo@haslo.ch>
2023-06-19 14:38:53 +0200

    Minor optimization, also added fields for core and guns

--- Assets/Scripts/Agents/ShooterBehaviour.cs
+++ Assets/Scripts/Agents/ShooterBehaviour.cs
@@ -2,21 +2,31 @@ using UnityEngine;
 
 public class ShooterBehaviour : MonoBehaviour {
     private GameObject currentTarget;
+    private bool hasTarget;
+
+    [SerializeField] private GameObject turretCore;
+    [SerializeField] private GameObject turretGun;
+
+    private void Start() {
+        hasTarget = false;
+    }
 
     private void OnTriggerEnter(Collider other) {
-        if (currentTarget == null && other.gameObject.CompareTag("Mob")) {
+        if (!hasTarget && other.gameObject.CompareTag("Mob")) {
             currentTarget = other.gameObject;
+            hasTarget = true;
         }
     }
 
     private void OnTriggerExit(Collider other) {
         if (other.gameObject == currentTarget) {
             currentTarget = null;
+            hasTarget = false;
         }
     }
 
     private void Update() {
-        if (currentTarget != null) {
+        if (hasTarget) {
             this.transform.LookAt(currentTarget.transform.position);
         }
     }
